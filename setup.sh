c49815c98fccd437c99d4943adf1ae603f20866e
Daniil <dankoselevs@gmail.com>
2022-03-27 18:31:04 +0300

    added ly

--- setup.sh
+++ setup.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 #
+copy_stuff(){
+    cp -rf .config ~
+    cp .xbindkeysrc ~
+    cp .zshrc ~
+}
+
+download_install_ly(){
+    mkdir ly_install
+    cd ly_install
+    git clone --recurse-submodules https://github.com/nullgemm/ly.git
+    cd ly
+    make
+    sudo make install
+    sudo systemctl enable ly.service
+}
+
 clear
 cat <<- EOF
 [*] Installing packages...
 [1] Install reqired
 [2] Intall all(PyCharm, OnlyOffice)
 
-EOF
-    
+EOF 
 read -p "[?] Select Option : "
 if [ $REPLY = "1" ]; then
 paru -S qtile xbindkeys rofi polybar nitrogen pcmanfm neovim
-cp .config ~/.config
 elif [ $REPLY = "2" ]; then
 paru -S qtile xbindkeys rofi polybar nitrogen pcmanfm neovim onlyoffice-bin pycharm-professional
-cp .config ~/.config
 else
 echo -e "[!] Invalid Option, Exiting...  Your option - $REPLY"
 exit 1
 fi
+
+copy_stuff
+download_install_ly
