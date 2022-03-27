33dbc576741cf04d99e69f54ba4f763e68d0ca88
Daniil on VM <dankoselevs@gmail.com>
2022-03-27 22:28:28 +0300

    Added zsh and modidied setup even more

--- setup.sh
+++ setup.sh
@@ -3,13 +3,8 @@
 
 DIR=`pwd`
 FDIR="$HOME/.local/share/fonts"
-PDIR="$HOME/.config/polybar"
 
-cp -rf .config ~
-cp .xbindkeysrc ~
-cp .zshrc ~
-
-
+#Installing login manager(Ly)
 mkdir ly_install
 cd ly_install
 git clone --recurse-submodules https://github.com/nullgemm/ly.git
@@ -18,18 +13,14 @@ make
 sudo make install
 sudo systemctl enable ly.service
 
-
-clear
-#cat <<- EOF
-#[*] Installing packages...
-#[1] Install reqired
-#[2] Intall all(PyCharm, OnlyOffice)
-
-#EOF 
-paru -S qtile xbindkeys rofi 1nitrogen pcmanfm neovim xorg alacritty firefox ttf-spacemono ttf-font-awesome python-pip ttf-icomoon-feather
+#installing packages
+paru -S qtile xbindkeys rofi nitrogen pcmanfm neovim xorg alacritty firefox ttf-spacemono python-pip zsh starship
 pip install neovim 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+chsh -s /bin/zsh
 
 #installing fonts
 echo -e "\n[*] Installing fonts..."
@@ -39,3 +30,8 @@ else
 	mkdir -p "$FDIR"
 	cp -rf $DIR/fonts/* "$FDIR"
 fi
+
+#Copying config files
+cp -rf .config ~
+cp .xbindkeysrc ~
+cp .zshrc ~
